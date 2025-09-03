5348a06d4b47c5dca88037da4380ac4d2d1aa8aa
Sainag <saigadangi@gmail.com>
2025-09-03 22:13:48 +0530

    fix: Let the redirect endpoint be /shortCode

--- src/routes/url.route.ts
+++ src/routes/url.route.ts
@@ -2,13 +2,11 @@ import { Router } from "express";
 import {
   createShortUrl,
   getDataByShortCode,
-  redirectToOriginalUrl,
 } from "../controllers/url.controller";
 
 const router = Router();
 // the route /
 
-router.get("/:shortCode", redirectToOriginalUrl);
 router.post("/urls", createShortUrl);
 router.get("/urls/:shortCode", getDataByShortCode);
 
